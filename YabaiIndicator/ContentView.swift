f00d55d8efe33766f9d8834cdd12effe92078f48
Max Zhao <alcasa.mz@gmail.com>
2021-12-26 23:16:04 +0100

    Initial Commit

---
+++ YabaiIndicator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  YabaiIndicator
+//
+//  Created by Max Zhao on 26/12/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ YabaiIndicator/YabaiIndicatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  YabaiIndicatorApp.swift
+//  YabaiIndicator
+//
+//  Created by Max Zhao on 26/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct YabaiIndicatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
